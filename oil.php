11768ba6602ea5cbd3c417ee9009162a476c1a4c
ROCKSTAR MAHI <maheshbandaru1974@gmail.com>
2024-06-27 20:43:43 +0530

    Initial Commit by Mahesh

---
+++ oil.php
@@ -0,0 +1,47 @@
+<!doctype html5>
+<html>
+<head></head>
+<title>oil</title>
+<style>
+h1{
+color: red}
+div{	
+text-align: left;
+}
+</style>
+<body>
+<h1 style="text-align: center">OIL PAINTINGS</h1>
+</div>
+<div class="input-wrapper">
+<input type="text" value="NATURE" readonly style="text-align: center"/><br>
+<img src="https://render.fineartamerica.com/images/rendered/default/flatrolled/yoga-mat/images/artworkimages/medium/3/indian-oil-paintings-for-sale-famous-oil-painting-artist-ramyasadasivam-ramya-sadasivam.jpg?&targetx=261&targety=-55&imagewidth=798&imageheight=551&modelwidth=1320&modelheight=440&backgroundcolor=9FA599&orientation=1&producttype=yogamat" height= 50% width= 25%><br>
+view of nature<br>
+by ramyasadasivam<br>
+Rs.600<br><br>
+</div>
+<div class="input-wrapper">
+<input type="text" value="BUTTERFLY" readonly style="text-align: center"/><br><br>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrCum5RiNd9AMOYwJ9V47jkogEi09S0-ayvQ&usqp=CAU" height= 50% width= 25%><br>
+Art of Beauty<br>
+by Machin<br>
+Rs.1200<br><br>
+<div class="input-wrapper">
+<input type="text" value="Rain" readonly style="text-align: center"/><br><br>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmtb2VpQTQOkpe-OX-IG_Q3xosBM9zQKqE7g&usqp=CAU" height= 50% width= 25%><br>
+Art of World<br>
+by Jhon dob<br>
+Rs.500<br><br>
+<div class="input-wrapper">
+<input type="text" value="Surrealism" readonly style="text-align: center"/><br><br>
+<img src="https://i.pinimg.com/736x/0f/68/b8/0f68b8875c2c6b46384bca309d480ee0.jpg" height= 50% width= 25%><br>
+Reality shower<br>
+by Micheal Cheval<br>
+Rs.10000<br><br>
+<div class="input-wrapper">
+<input type="text" value="Colour World" readonly style="text-align: center"/><br><br>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmbVnHDbhOgRXbjqChEWUF6NbXrxmnlHtsNDKhFX3WjlXG91CwROf2F8gAoV2i1UgTAUY&usqp=CAU" height= 50% width= 25%><br>
+World Full of colours<br>
+by Raghav Shastry<br>
+Rs.400<br><br>
+</body>
+</html>
